8cd6f6494441192d5471b4fa307ef42aaee63afe
unixpariah <oskar.rochowiak@tutanota.com>
2024-05-10 16:12:10 +0200

    removed zig-yaml
    removed zig-yaml
    
    removed zig-yaml

--- build.zig
+++ build.zig
@@ -23,7 +23,7 @@ pub fn build(b: *std.Build) void {
 
     const xkbcommon = b.createModule(.{ .root_source_file = .{ .path = "deps/zig-xkbcommon/src/xkbcommon.zig" } });
 
-    const yaml = b.createModule(.{ .root_source_file = .{ .path = "deps/zig-yaml/src/yaml.zig" } });
+    const yaml = b.createModule(.{ .root_source_file = .{ .path = "deps/yaml/src/yaml.zig" } });
 
     const exe = b.addExecutable(.{
         .name = "seto",
