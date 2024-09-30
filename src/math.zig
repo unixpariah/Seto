3af0db959e4a6233bf3703f59110b9bce2c75e9f
unixpariah <oskar.rochowiak@tutanota.com>
2024-09-30 21:37:30 +0200

    transform cleanup

--- src/math.zig
+++ src/math.zig
@@ -3,23 +3,22 @@ const std = @import("std");
 pub const Mat4 = [4][4]f32;
 pub const Vec4 = [4]f32;
 
-pub fn transform(font_size: f32, m1: Vec4) Mat4 {
-    var result: Mat4 = undefined;
+pub fn transform(font_size: f32, v4: Vec4) Mat4 {
     var vx = @shuffle(f32, [4]i32{ 0, 0, 0, 1 }, undefined, [4]i32{ 0, 0, 0, 0 });
     var vy = @shuffle(f32, [4]i32{ 0, 0, 0, 1 }, undefined, [4]i32{ 1, 1, 1, 1 });
     var vw = @shuffle(f32, [4]i32{ 0, 0, 0, 1 }, undefined, [4]i32{ 3, 3, 3, 3 });
     vx = vx * [4]f32{ 1, 0, 0, 0 };
     vy = vy * [4]f32{ 0, 1, 0, 0 };
-    vw = vw * m1;
+    vw = vw * v4;
     vy = vy + vw;
     vx = vx + vy;
-    result[3] = vx;
 
-    result[0] = .{ font_size, 0, 0, 0 };
-    result[1] = .{ 0, font_size, 0, 0 };
-    result[2] = .{ 0, 0, 0, 0 };
-
-    return result;
+    return .{
+        .{ font_size, 0, 0, 0 },
+        .{ 0, font_size, 0, 0 },
+        .{ 0, 0, 0, 0 },
+        .{ vx[0], vx[1], 0, 1 },
+    };
 }
 
 pub fn translate(x: f32, y: f32) Vec4 {
